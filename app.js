8171cc5155ead3d418c52ad56a12ad7d77a58a19
dark1030 <poovarasanarumugam1030@gmail.com>
2024-02-15 18:04:25 +0530

    animation added

--- app.js
+++ app.js
@@ -19,3 +19,34 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+gsap.to(".bgAnim1,.bgAnim2", {
+  x: -100,
+  duration: 2,
+});
+
+gsap.set(".wlcmImg", {
+  opacity: 0,
+});
+
+gsap.to(".wlcmImg", {
+  opacity: 1,
+  duration: 3,
+  x: 20,
+});
+
+gsap.set(".box", {
+  opacity: 0,
+});
+
+gsap.to(".box", {
+  opacity: 1,
+  duration: 1,
+  yoyo: true,
+  ease: "power1",
+  stagger: {
+    amount: 2,
+    each: 0.5,
+    from: 0,
+  },
+});
