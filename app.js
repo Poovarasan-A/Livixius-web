ec8f739ee5e0196816bea32cb2b559346b8964ec
dark1030 <poovarasanarumugam1030@gmail.com>
2024-02-16 13:18:03 +0530

    mobile view added

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//---------------------------------menu navigation -----------------------------
+let menu = document.querySelector(".menu");
+let menuList = document.querySelector("nav ul");
+
+menu.addEventListener("click", () => {
+  menuList.classList.toggle("showmenu");
+});
+
 // ------------------------------ background circle animation------------------------
 gsap.to(".bgAnim1,.bgAnim2", {
   x: -100,
@@ -37,23 +46,62 @@ gsap.to(".wlcmImg", {
 });
 
 // ------------------------------ features box animation------------------------
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".box",
-    start: "-40% center",
-    end: "30% center",
-    scrub: true,
-    markers: false,
-  },
-});
-tl.set(".box", {
-  opacity: 0,
-  duration: 3,
-});
-tl.to(".box", {
-  opacity: 1,
-  duration: 3,
-  y: -50,
-});
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".box",
+//     start: "-40% center",
+//     end: "30% center",
+//     scrub: true,
+//     markers: false,
+//   },
+// });
+// tl.set(".box", {
+//   opacity: 0,
+//   duration: 3,
+// });
+// tl.to(".box", {
+//   opacity: 1,
+//   duration: 3,
+//   y: -50,
+// });
 
+AOS.init({
+  delay: 300,
+  duration: 1200,
+  once: false,
+});
 // ------------------------------ about page circle animation------------------------
+
+let tl2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".circle1",
+      start: "-200% center",
+      end: "80% center",
+      scrub: 6,
+      markers: false,
+    },
+  })
+  .to(".circle1", {
+    y: -450,
+    duration: 3,
+    backgroundColor: "#ab9ff2d8",
+    yoyo: true,
+  });
+
+let tl3 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".circle2",
+      start: "60% center",
+      end: "340% center",
+      scrub: 6,
+      markers: false,
+    },
+  })
+  .to(".circle2", {
+    y: 450,
+    duration: 3,
+    backgroundColor: "#ab9ff228",
+    yoyo: true,
+  });
